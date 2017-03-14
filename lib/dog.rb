c6d6733e9a3dff97d07ab8259d2fdbd76d5b14e7
Sylvia Lee <helloimsylvia@gmail.com>
2017-03-14 16:05:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name #getter
+    @name
+  end
+
+  def name=(dog_name) #setter
+    @name = dog_name
+  end
+
+  def breed #getter
+    @breed
+  end
+
+  def breed=(breed) #setter
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name #getter
+    @name
+  end
+
+  def name=(new_name) #setter
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+end
